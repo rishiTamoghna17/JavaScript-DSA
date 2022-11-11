7284e143196d078694da569662c8581542c036e7
rishiTamoghna17 <tamoghna171099@gmail.com>
2022-11-11 07:54:32 +0530

    frequency_count

--- anagram.js
+++ anagram.js
@@ -1,19 +1,35 @@
 function anagram (str1,str2){
-    arr1 = new Array(26).fill(0);
-    arr2 = new Array(26).fill(0);
-    for(let char of str1){
-        arr1[char.charCodeAt(0)-'a'.charCodeAt(0)]++;
+    // arr1 = new Array(26).fill(0);
+    // arr2 = new Array(26).fill(0);
+    // for(let char of str1){
+    //     arr1[char.charCodeAt(0)-'a'.charCodeAt(0)]++;
+    // }
+    // for(let char of str2){
+    //     arr2[char.charCodeAt(0)-'a'.charCodeAt(0)]++;
+    // }
+    // for(let i = 0; i < 26; i++){
+    //     if(arr1[i]!==arr2[i]) return false;
+    // }
+    // return true;
+
+
+    //.................onother aproch..........//
+
+    if(str1.length!==str2.length){
+        return false;
     }
-    for(let char of str2){
-        arr2[char.charCodeAt(0)-'a'.charCodeAt(0)]++;
+    let obj ={};
+    for(let item of str1){
+        obj[item]=(obj[item] || 0)+1;
     }
-    for(let i = 0; i < 26; i++){
-        if(arr1[i]!==arr2[i]) return false;
+    for(let item of str2){
+        if(!obj[item]) return false;
+        obj[item]-=1;
     }
     return true;
 
 }
-let str1 = "cta";
-let str2 = "cat";
+let str1 = "ctt";
+let str2 = "tct";
 let res = anagram(str1,str2)
 console.log(res)
